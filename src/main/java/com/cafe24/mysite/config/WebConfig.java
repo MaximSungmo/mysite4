f91f2b3826401b0f3e9535ebc113ad0a0f828c9c
bit <bit@DESKTOP-H5F1754>
2019-06-03 16:04:30 +0900

    import securityconfig변경

--- src/main/java/com/cafe24/mysite/config/WebConfig.java
+++ src/main/java/com/cafe24/mysite/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.cafe24.mysite.config;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -10,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.cafe24.config.web.FileuploadConfig;
 import com.cafe24.config.web.MVCConfig;
 import com.cafe24.config.web.MessageConfig;
+import com.cafe24.config.web.SecurityConfig;
 
 @Configuration
 // configDriven
