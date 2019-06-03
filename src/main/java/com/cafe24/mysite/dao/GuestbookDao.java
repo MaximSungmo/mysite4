d7caae6b4a2e4d8cf7263dd87ba34878f1d0a588
bit <bit@DESKTOP-H5F1754>
2019-06-03 16:02:16 +0900

    init commit , jenkins upload용

---
+++ src/main/java/com/cafe24/mysite/dao/GuestbookDao.java
@@ -0,0 +1,36 @@
+package com.cafe24.mysite.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cafe24.mysite.vo.GuestbookVo;
+
+@Repository
+public class GuestbookDao {
+	
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public Boolean delete(GuestbookVo vo) {
+		int count = sqlSession.delete("guest.delete", vo);		
+		return 1 == count;
+	}
+	
+	public Boolean insert(GuestbookVo vo) {
+		int count = sqlSession.insert("guest.insert", vo);
+		return 1 == count;
+	}	
+	
+	public List<GuestbookVo> getList(){
+		List<GuestbookVo> result = sqlSession.selectList("guest.getList");
+		return result;
+	}	
+	
+}
---
+++ src/main/java/com/cafe24/mysite/initializer/MysiteWebApplicationInitializer.java
@@ -0,0 +1,48 @@
+package com.cafe24.mysite.initializer;
+
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.cafe24.mysite.config.AppConfig;
+import com.cafe24.mysite.config.WebConfig;
+
+public class MysiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new CharacterEncodingFilter("UTF-8", true) };
+		
+	}
+
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet dispatcherServlet = (DispatcherServlet)(super.createDispatcherServlet(servletAppContext));
+		
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(false);
+		return dispatcherServlet;
+		
+	}
+
+}
